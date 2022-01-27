7ed0ec5c8ab720e7d57ae47fdde47e5b422d92b2
Tina Stanczyk <cstanczyk123@gmail.com>
2022-01-27 18:35:48 -0500

    created seed data and files

---
+++ seeds/seed.js
@@ -0,0 +1,31 @@
+const sequelize = require('../config/connection');
+const { User, Blog, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+    });
+  };
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+    });
+  };
+
+  process.exit(0);
+};
+
+seedDatabase();
